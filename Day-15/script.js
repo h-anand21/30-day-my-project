04224c3a5abe037e19e2124fce8e974133ac9e13
Himanshu Anand <himanshuanand563@gmail.com>
2026-04-11 23:03:02 +0530

    js and todays complited

--- Day-15/script.js
+++ Day-15/script.js
@@ -2,3 +2,13 @@ const toggleBtn = document.getElementById('toggleBtn');
 const body = document.body;
 const icon = document.getElementById('icon');
 
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark');
+
+  // Change icon
+  if (body.classList.contains('dark')) {
+    icon.src = 'moon.png';
+  } else {
+    icon.src = 'sun.png';
+  }
+});
