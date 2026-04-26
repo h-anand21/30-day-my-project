1e71b6bbb011dee84c4e0d0ba7d3ae979ec027de
Himanshu Anand <himanshuanand563@gmail.com>
2026-04-26 18:06:59 +0530

    js done my  30 day challenge done

---
+++ Day-30/script.js
@@ -0,0 +1,21 @@
+let speech = new SpeechSynthesisUtterance();
+
+let voices= []
+
+let voiceSecector = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice , i) => (voiceSecector.options[i] = new Option(voice.name , i)));
+    
+
+}
+
+voiceSecector
+
+document.querySelector('button').addEventListener('click', () => {
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+});
